c3212b65fd2fdbe6118319e20df683ceaa92a222
Hatem902 <hatelm902@gmail.com>
2023-06-02 10:12:42 +0100

    feature : finished personality detection functionality

---
+++ helpers/backend/get-users.js
@@ -0,0 +1,93 @@
+export const getUser = [
+  {
+    user_id: 'onsjabeur',
+    personality: 'ISTP : Introvert, Sensing, Thinking, Perceiving',
+    posts: 391,
+    likes: 10954,
+  },
+  {
+    user_id: 'onsjabeur',
+    personality: 'INTP : Introvert, Intuitive, Thinking, Perceiving',
+    posts: 391,
+    likes: 10954,
+  },
+  {
+    user_id: 'onsjabeur',
+    personality: 'ISTP : Introvert, Sensing, Thinking, Perceiving',
+    posts: 391,
+    likes: 10954,
+  },
+  {
+    user_id: 'ardeona',
+    personality: 'ESTP : Extrovert, Sensing, Thinking, Perceiving',
+    posts: 246,
+    likes: 42,
+  },
+  {
+    user_id: 'ardeona',
+    personality: 'ISTP : Introvert, Sensing, Thinking, Perceiving',
+    posts: 246,
+    likes: 42,
+  },
+  {
+    user_id: 'ardeona',
+    personality: 'ESTP : Extrovert, Sensing, Thinking, Perceiving',
+    posts: 246,
+    likes: 42,
+  },
+  {
+    user_id: 'chay1',
+    personality: 'INTP : Introvert, Intuitive, Thinking, Perceiving',
+    posts: 307,
+    likes: 26,
+  },
+  {
+    user_id: 'chay1',
+    personality: 'ISTP : Introvert, Sensing, Thinking, Perceiving',
+    posts: 307,
+    likes: 26,
+  },
+  {
+    user_id: 'chay1',
+    personality: 'ISTP : Introvert, Sensing, Thinking, Perceiving',
+    posts: 307,
+    likes: 26,
+  },
+
+  {
+    user_id: 'elmichelle_',
+    personality: 'INTJ : Introvert, Intuitive, Thinking, Judging',
+    posts: 601,
+    likes: 19,
+  },
+  {
+    user_id: 'elmichelle_',
+    personality: 'INTJ : Introvert, Intuitive, Thinking, Judging',
+    posts: 601,
+    likes: 19,
+  },
+  {
+    user_id: 'elmichelle_',
+    personality: 'INTJ : Introvert, Intuitive, Thinking, Judging',
+    posts: 601,
+    likes: 19,
+  },
+  /* {
+      user_id: 'frosty',
+      personality: 'ESTP : Extrovert, Sensing, Thinking, Perceiving',
+      posts: 44,
+      likes: 58,
+    },
+    {
+      user_id: 'frosty',
+      personality: 'ESTJ : Extrovert, Sensing, Thinking, Judging',
+      posts: 44,
+      likes: 58,
+    },
+    {
+      user_id: 'frosty',
+      personality: 'ESTP : Extrovert, Sensing, Thinking, Perceiving',
+      posts: 44,
+      likes: 58,
+    }, */
+];
